72b6616be88cd6b4793574bca05f00b96e802065
Kunal Patil <kunalpatil29th@gmail.com>
2026-09-02 12:26:11 +0530

    Updated project

---
+++ Js/app.js
@@ -0,0 +1,58 @@
+// let num1 = 10;
+// let num2 = 3;
+
+// let sum = num1 + num2;
+
+// console.log("sum :", sum);
+
+// Templalte literals
+// let a = 5;
+// let b = 10;
+// let c = 44;
+// console.log(`your pay ${a + b + c} ruppes` );
+
+// arthemtic operation
+// let a = 10;
+// let b = 20;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a%b);
+// console.log(a**b);
+// console.log(a/b);
+// console.log(a*b);
+ 
+// unary operator
+// console.log(a++);
+// console.log(++a);
+// console.log(++a)
+
+
+// assigment operator
+// let a = 10;
+// let b = 44;
+// b = a;
+// console.log(b);
+
+
+// camparsion operator
+
+// console.log("if we code before if");
+// let age = 18;
+// if(age >= 19){
+//     console.log("You can vote");
+// }
+// console.log("Code before if ");
+
+let str = "apple";
+if((str[0]=== 'a')&&(str.length > 3))
+{
+    console.log("Good");
+}
+else{
+    console.log("Not");
+}
+
+
+
+
+
