825e54935fd3c253e792a5c20fde0a7fd3af1dd3
Kunal Patil <kunalpatil29th@gmail.com>
2026-09-07 20:52:02 +0530

    updated project

---
+++ Function.js
@@ -0,0 +1,28 @@
+function username() {
+    console.log("Kunal");
+}
+
+username();
+username();
+username();
+username();
+
+
+function isadult() {
+    let age = 22;
+    if (age >= 18) {
+        console.log("adult");
+    }
+    else {
+        console.log("Not adult");
+    }
+}
+
+isadult();
+
+// print opeamm
+function poem() {
+    console.log("Twinkle, twinkle, little star,");
+}
+
+poem();
---
+++ java_script_objects_literals.js
@@ -0,0 +1,18 @@
+// const student = {
+//     name : "kunal",
+//     age  : 23,
+//     marks : 43.3,
+//     city : "delhi"
+
+// }
+const post = {
+    username: "KunalPatil",
+    content : " A content post is any digital material—like text, images, or videos—shared on social media or websites to inform, entertain, or engage an audience",
+    likes : 150,
+    reposts: 5,
+    tags:["@apnacollege" , "@atge"]
+
+    
+};
+
+post["content"]; //asccees the keyvaluepair
