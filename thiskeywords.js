1fe5d56db15a87f3ca3e144b9b7398365ded4323
Kunal Patil <kunalpatil29th@gmail.com>
2026-09-08 21:35:21 +0530

    updated project

--- thiskeywords.js
+++ thiskeywords.js
@@ -1,8 +1,12 @@
-const student = {
-    name : "Kunal",
-    age : 23,
-    eng : 66,
-    hindi : 55,
-    math : 44,
+// const student = {
+//     name : "Kunal",
+//     age : 23,
+//     eng : 66,
+//     hindi : 55,
+//     math : 44,
 
+// }
+
+const sum = (a,b)=>{
+    console.log(a+b);
 }
