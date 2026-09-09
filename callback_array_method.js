1c05ddf393cd05a3607973af67891e1bb00e0718
Kunal Patil <kunalpatil29th@gmail.com>
2026-09-09 11:57:20 +0530

    updated project

---
+++ callback_array_method.js
@@ -0,0 +1,28 @@
+// let  arr = [1, 3 ,4 ,, 5, 6 ];
+
+// arr.forEach((el)=>{
+//     console.log(el);
+
+// });
+// function print(el){
+//     console.log(el);
+// }
+// // arr.forEach(print);
+
+let arr = [{
+    name:"Kunal",
+    age : 34,
+},
+{
+    name: "BMW",
+    Modal: 3,
+},
+{
+
+}];
+
+// map function  method
+let num = [1,2,4 ,5,6 ];
+let double = num.map(function(el){
+    return el*2;
+});
