8352c4ae44a8deb3918d3b8a28d5658268551b0d
Kunal Patil <kunalpatil29th@gmail.com>
2026-09-10 20:56:07 +0530

    Initial commit

--- reduce.method.js
+++ reduce.method.js
@@ -1,34 +1,49 @@
-const myNums = [1, 2, 3]
+// const myNums = [1, 2, 3]
 
-// const myTotal = myNums.reduce(function (acc, currval) {
-//     console.log(`acc: ${acc} and currval: ${currval}`);
-//     return acc + currval
-// }, 0)
+// // const myTotal = myNums.reduce(function (acc, currval) {
+// //     console.log(`acc: ${acc} and currval: ${currval}`);
+// //     return acc + currval
+// // }, 0)
 
-const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+// const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
 
-console.log(myTotal);
+// console.log(myTotal);
 
 
-const shoppingCart = [
-    {
-        itemName: "js course",
-        price: 2999
-    },
-    {
-        itemName: "py course",
-        price: 999
-    },
-    {
-        itemName: "mobile dev course",
-        price: 5999
-    },
-    {
-        itemName: "data science course",
-        price: 12999
-    },
-]
+// const shoppingCart = [
+//     {
+//         itemName: "js course",
+//         price: 2999
+//     },
+//     {
+//         itemName: "py course",
+//         price: 999
+//     },
+//     {
+//         itemName: "mobile dev course",
+//         price: 5999
+//     },
+//     {
+//         itemName: "data science course",
+//         price: 12999
+//     },
+// ]
 
-const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+// const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
 
-console.log(priceToPay);
+// console.log(priceToPay);
+
+// find maximum array
+let num = [2,4,5,6 ,7, 8];
+
+let result = num.reduce((max,el)=>{
+
+
+    if(el > max){
+        return el;
+    }
+    else{
+        return max;
+    }
+  });
+  console.log(result);
