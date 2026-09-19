c4f2dac4446ad0c5aa2af261687a66bd37497b0d
Kunal Patil <kunalpatil29th@gmail.com>
2026-09-19 21:41:28 +0530

    Update HTML project

--- Advanced JavaScript/app.js
+++ Advanced JavaScript/app.js
@@ -1,6 +1,8 @@
 let form = document.querySelector("form");
 
-form.addEventListener("submit", function(){
+form.addEventListener("submit", function(event){
+    // event.preventDefault();
+    
     // console.log("form submitted");
     alert("form submitted");
 });
