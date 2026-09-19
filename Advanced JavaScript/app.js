c48c535ad1d1effe8360dc223189b6ff5f81f39b
Kunal Patil <kunalpatil29th@gmail.com>
2026-09-19 21:27:01 +0530

    Update HTML project

---
+++ Advanced JavaScript/app.js
@@ -0,0 +1,20 @@
+let inp = document.querySelector('input');
+
+// keydown 
+
+// inp.addEventListener("keydown", function(){
+//     console.log("press the butoon");
+// });
+
+
+// keypress
+// inp.addEventListener("keypress",function(){
+//     console.log("key was released");
+// });
+
+//
+inp.addEventListener("keydown",function(){
+    console.log("key is downword");
+
+});
+
