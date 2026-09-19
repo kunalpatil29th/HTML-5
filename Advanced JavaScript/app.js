63826fe2b3ff27e9d7897eab11d4efc29aa5139e
Kunal Patil <kunalpatil29th@gmail.com>
2026-09-19 21:07:43 +0530

    Update HTML project

--- Advanced JavaScript/app.js
+++ Advanced JavaScript/app.js
@@ -1,20 +1,7 @@
-let btn = document.querySelector("button");
-btn.addEventListener("click", function(){
-    console.log("genrate random color");
-});
+let btn = document.querySelector('button');
 
-
-function getRandomColor(){
-    let red = Math.floor(Math.random()*255);
-    let green = Math.floor(Math.random()*255);
-    let blue = Math.floor(Math.random()*255);
-
-
-    let color = `(${red},${green}, ${blue})`;
-    return color;
-
-
-
-}
-
+btn.addEventListener('click',function(event){
+    console.log(event);
+    console.log('click button');
+})
 
