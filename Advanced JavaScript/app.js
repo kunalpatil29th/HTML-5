9a7d405a38a93d217590a28912aee0308a8e1e86
Kunal Patil <kunalpatil29th@gmail.com>
2026-09-19 21:38:35 +0530

    Update HTML project

--- Advanced JavaScript/app.js
+++ Advanced JavaScript/app.js
@@ -1,20 +1,12 @@
-let inp = document.querySelector('input');
+let form = document.querySelector("form");
 
-// keydown 
-
-// inp.addEventListener("keydown", function(){
-//     console.log("press the butoon");
-// });
-
-
-// keypress
-// inp.addEventListener("keypress",function(){
-//     console.log("key was released");
-// });
-
-//
-inp.addEventListener("keydown",function(){
-    console.log("key is downword");
-
+form.addEventListener("submit", function(){
+    // console.log("form submitted");
+    alert("form submitted");
 });
+
+
+
+
+
 
